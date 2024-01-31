3da85a928085578f592a2a90af6638857c7dbe49
pxmpkeen <pxmpkeen@gmail.com>
2024-02-01 00:42:40 +0400

    pyBetty fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or type(roman_string) != str:
         return 0
     sum = 0
     prev = 1000
