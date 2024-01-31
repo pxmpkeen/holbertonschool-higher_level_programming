a2b31ac06f539a5d3ab5bc27a4ef59bac60336ef
pxmpkeen <pxmpkeen@gmail.com>
2024-02-01 00:40:27 +0400

    pyBetty fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,20 +14,3 @@ def roman_to_int(roman_string):
                 sum -= 2 * curr
         sum += curr
     return sum
-
-
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
