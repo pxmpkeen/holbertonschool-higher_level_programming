801a7d9a3c75b5e703661880ca4f4807bddc4d60
pxmpkeen <pxmpkeen@gmail.com>
2024-02-03 22:37:08 +0400

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """adding ints"""
-    if isinstance(a, (int, float)) == False:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)) == False:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
