b8eee4dad3c4a6fe4dbe33277ccd5383a28a082c
pxmpkeen <pxmpkeen@gmail.com>
2024-02-03 19:55:09 +0400

    hot fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Declare class Square"""
 
+
 class Square:
     """Square class with a private attribute"""
-    def __init__(self, size = 0):
-        try:
-            self.__size = int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+    def __init__(self, size=0):
+        self.__size = int(size)
+        if not isinstance(size, int):
+            raise ValueError("size must be >= 0")
+        elif size < 0:
+            raise TypeError("size must be an integer")
