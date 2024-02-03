7decb14089956987c1579c144361cb0d8d19680e
pxmpkeen <pxmpkeen@gmail.com>
2024-02-03 19:56:58 +0400

    hot fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,8 +5,8 @@
 class Square:
     """Square class with a private attribute"""
     def __init__(self, size=0):
-        self.__size = int(size)
+        self.__size = size
         if not isinstance(size, int):
-            raise ValueError("size must be >= 0")
-        elif size < 0:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
