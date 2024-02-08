f25d94b4d663fb57682fec832c4339c54d5854ea
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 11:23:56 +0400

    where is a problem

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -84,10 +84,3 @@ class Rectangle:
     def square(cls, size=0):
         """Creation a square"""
         return cls(size, size)
-
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square
-
-        )i
