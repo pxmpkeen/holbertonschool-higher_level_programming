d711482287179418c821e3d98d8443c319b80425
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 16:33:40 +0400

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-list_of_args = sys.args[1:]
+list_of_args = sys.argv[1:]
 save_to_json_file(list_of_args, filename)
 load_from_json_file(filename)
