7e5c84b2355c97acf8a338fcde0ce67c5215413a
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 17:53:23 +0400

    hoping

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,6 +26,6 @@ class Student:
         """Changing"""
         if not json:
             return
-        for i in json:
-            if i in Student.__dict__:
-                self.i = json[i]
+        for i in Students.__dict__:
+            if str(i) in json:
+                self.i = json[str(i)]
