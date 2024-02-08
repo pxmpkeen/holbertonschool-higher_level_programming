c84d333966be3c2b48b59e4c94b30315ba1a54fe
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 14:39:02 +0400

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -3,20 +3,15 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square class"""
 
     def __init__(self, size):
         """init of square class"""
         self.integer_validator("size", size)
-        self.__width = self.__height = size
+        self.__size = size
+        super().__init__(size, size)
 
-    def area(self):
-        """Implementation of area method"""
-        return self.__width * self.__height
-
-
-s = Square(13)
-
-print(s)
-print(s.area())
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size)
