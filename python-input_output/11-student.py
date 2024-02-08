62f11dfc3724d2a9ce5056e48169071fad6847b8
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 17:43:08 +0400

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ToJson"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
+
+    def reload_from_json(self, json):
+        """Changing"""
+        for i in json:
+            if i in Student.__dict__:
+                self.i = json[i]
