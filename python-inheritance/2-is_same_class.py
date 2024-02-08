afe3793870fd7ef2af44d6f6088d75b8662eae57
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 11:59:51 +0400

    hot fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,4 +3,5 @@
 
 
 def is_same_class(obj, a_class):
+    """Docs"""
     return type(obj) is a_class:
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""just doc"""
+
 
 def is_kind_of_class(obj, a_class):
     return isinstance(obj, a_class)
