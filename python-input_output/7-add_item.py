6b0a4a653ee1c6a3446b65f3300fff41566761a2
pxmpkeen <pxmpkeen@gmail.com>
2024-02-08 16:46:05 +0400

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,17 @@
 """JSON"""
 import json
 import sys
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-list_of_args += sys.argv[1:]
+if os.path.isfile(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+list_of_args.extend(sys.argv[1:])
 save_to_json_file(list_of_args, filename)
-load_from_json_file(filename)
