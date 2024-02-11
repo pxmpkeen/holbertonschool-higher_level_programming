04d8d5a7a7444cd035d1e7c3b0084a11548d5e03
pxmpkeen <pxmpkeen@gmail.com>
2024-02-12 02:46:31 +0400

    Square added

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Inheriting Square from Rectangle
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherited from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization of object"""
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """toStr method"""
+        sId = "[Square] ({}) ".format(self.id)
+        return sId + "{}/{} - {}".format(self.x, self.y, self.size)
