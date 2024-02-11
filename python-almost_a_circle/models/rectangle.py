7703d639d6014a13bc970b4637d3a864bc417679
pxmpkeen <pxmpkeen@gmail.com>
2024-02-12 01:12:36 +0400

    attribute validating added

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -40,19 +40,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """width setter"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @height.setter
     def height(self, height):
         """height setter"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @x.setter
     def x(self, x):
         """x setter"""
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @y.setter
     def y(self, y):
         """y setter"""
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
