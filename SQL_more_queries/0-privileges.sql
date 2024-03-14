44692ca55e3c13c62ae79ad4730387b365a9ae50
pxmpkeen <pxmpkeen@gmail.com>
2024-03-14 18:14:31 +0400

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost', 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
