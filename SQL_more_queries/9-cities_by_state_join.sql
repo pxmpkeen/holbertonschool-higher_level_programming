f744ec058770420ae2b246c869745d5edce3201d
pxmpkeen <pxmpkeen@gmail.com>
2024-03-14 19:32:32 +0400

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- join
-SELECT cities.id AS id, cities.name AS state_id, states.name AS name FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id;
+SELECT cities.id AS id, cities.name AS state_id, states.name AS name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC;
