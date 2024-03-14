d10c220960df38afbfe7591c96bd0816d5bdd1d4
pxmpkeen <pxmpkeen@gmail.com>
2024-03-14 17:10:50 +0400

    102-top_city.sql

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- temp
-SELECT city, AVG(value) as avg_temp from temperatures where month => 7 and month <= 8 group by city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp from temperatures where month in (7, 8) group by city ORDER BY avg_temp DESC LIMIT 3;
 
