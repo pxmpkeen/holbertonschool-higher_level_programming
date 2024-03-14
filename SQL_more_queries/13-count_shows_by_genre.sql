dc9d597910e20ff9f9958d962c52ea78ba50ff8c
pxmpkeen <pxmpkeen@gmail.com>
2024-03-14 20:49:38 +0400

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- genres
-SELECT g.name AS genre, count(sg.show_id) AS number_of_shows FROM tv_genres g LEFT JOIN tv_show_genres sg ON sg.genre_id = g.id GROUP BY g.name ORDER BY count(sg.show_id) DESC;
+SELECT g.name AS genre, count(sg.show_id) AS number_of_shows FROM tv_genres g LEFT JOIN tv_show_genres sg ON sg.genre_id = g.id GROUP BY g.name ORDER BY 2 DESC;
