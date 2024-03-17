255ada815bd3e27df1080f8db64a59f68d8b94b4
pxmpkeen <pxmpkeen@gmail.com>
2024-03-18 01:25:56 +0400

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,8 +15,11 @@ if __name__ == "__main__":
             db=argv[3]
             )
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id""")
-    rows = cur.fetchall()
+    try:
+        cur.execute("""SELECT * FROM states ORDER BY id""")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(e)
 
     for row in rows:
         print(row)
