17fbd0c46f0cd91d03507fa27ecce24b62876af8
pxmpkeen <pxmpkeen@gmail.com>
2024-03-22 22:56:21 +0400

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,31 +7,11 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    word = argv[4]
-    words = word.split(' ')
-    if len(words) > 1:
-        word = words[0]
+    cur.execute("""SELECT * FROM `states`""")
 
-    try:
-        query = """
-        SELECT * FROM states WHERE name='{:s}' ORDER BY states.id
-        """
-        cur.execute(query.format(word)
-        rows = cur.fetchall()
-    except MySQLdb.Error as e:
-        print(e)
-
-    for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+    [print(state) for state in c.fetchall() if state[1] == argv[4]]
 
     cur.close()
     db.close()
